949112869ebd9f007a312039b9f3ac7526c5e044
Vinicius Fantinatto <vifanti@gmail.com>
2019-03-25 11:38:47 -0300

    Tutorial Angular finalizado

--- 2praticasProgramacaoWeb/angular-tour-of-heroes/src/app/app.module.ts
+++ 2praticasProgramacaoWeb/angular-tour-of-heroes/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -14,16 +18,25 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
-  bootstrap: [ AppComponent ]
-})
-export class AppModule { }
+    AppRoutingModule,
+    HttpClientModule,
+    
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+      )
+    ],
+    providers: [
+      // no need to place any providers due to the `providedIn` flag...
+    ],
+    bootstrap: [ AppComponent ]
+  })
+  export class AppModule { }
